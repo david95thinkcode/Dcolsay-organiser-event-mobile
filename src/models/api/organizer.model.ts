cbdf99d1da15c1ff1b2c52d0ba679185383def7a
David H <davidhihea@gmail.com>
2017-10-27 00:51:22 +0100

    [ Additions ] Model class added : - AccountOrganizer - Account - Event - Organizer - Ticket

---
+++ src/models/api/organizer.model.ts
@@ -0,0 +1,17 @@
+export class Organizer {
+    id: number;
+    name: string;
+    description: string;
+    email: string;
+    phone: string;
+
+    facebook: string;
+    twitter: string;
+    logo_path: string;
+
+    is_email_confirmed: boolean;
+    
+    constructor(parameters) {
+        
+    }
+}
