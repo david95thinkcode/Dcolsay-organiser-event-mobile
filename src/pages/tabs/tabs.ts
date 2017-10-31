20a47474fe8a5e8a282656c8660688b0243df095
David K. Hiheaglo <davidhihea@gmail.com>
2017-10-31 23:29:22 +0100

    [ Updated ]

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
-import { EventsListPage } from "../events/events-list/events-list";
+// import { EventsListPage } from "../events/events-list/events-list";
 import { TicketsListPage } from "../tickets/ticket-list/tickets-list";
+import { EventCategoryListPage } from "../events/event-category-list/event-category-list";
 import { AccountPage } from "../account/account";
 
 @Component({
@@ -11,7 +12,7 @@ import { AccountPage } from "../account/account";
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = EventsListPage;
+  tab2Root = EventCategoryListPage;
   tab3Root = TicketsListPage;
   tab4Root = AccountPage;
   
